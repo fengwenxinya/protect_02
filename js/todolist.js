5cd050d8e80e05903c5f5bb9e009d7a43bac6223
liTao <1091181083@qq.com>
2023-06-28 13:25:56 +0800

    新建了js文件

---
+++ js/todolist.js
@@ -0,0 +1,77 @@
+$(function() {
+    load();
+    $('#title').on('keydown', function(event) {
+        if (event.keyCode === 13) {
+            if ($(this).val() === '') {
+
+            } else {
+                var local = getDate();
+                console.log(local);
+                local.push({ title: $(this).val(), done: false });
+                setDate(local);
+                load();
+                $(this).val('');
+            }
+
+        }
+    });
+
+    $('ol,ul').on('click', 'a', function() {
+        var data = getDate();
+        var index = $(this).attr('id');
+        console.log(index);
+
+        data.splice(index, 1);
+        setDate(data);
+        load();
+    });
+
+    $('ol,ul').on('click', 'input', function() {
+        var data = getDate();
+        var index = $(this).siblings('a').attr('id');
+        // console.log(index);
+        data[index].done = $(this).prop('checked');
+        // console.log(data)
+
+        setDate(data);
+        load();
+
+    });
+
+
+
+    function getDate() {
+        var data = localStorage.getItem('todolist');
+        if (data !== null) {
+
+            return JSON.parse(data)
+        } else {
+            return []
+        }
+    };
+
+
+
+    function setDate(data) {
+        localStorage.setItem('todolist', JSON.stringify(data));
+    };
+
+    function load() {
+        var data = getDate();
+        $('ol,ul').empty();
+        var todocount = 0;
+        var donecount = 0;
+        $.each(data, function(i, n) {
+            if (n.done) {
+                $('ul').prepend('<li><input type="checkbox" checked = "checked"><p>' + n.title + '</p><a href="javascript:;" id = ' + i + '></a></li>')
+                donecount++;
+            } else {
+                $('ol').prepend('<li><input type="checkbox"><p>' + n.title + '</p><a href="javascript:;" id = ' + i + '></a></li>')
+                todocount++;
+            }
+
+        })
+        $('#todocount').text(todocount);
+        $('#donecount').text(donecount);
+    }
+})
